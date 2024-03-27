a633052332efcbf8c7410164941679d7c5a0fd83
@umang.t <umang.t@frendy.in>
2024-03-27 22:39:27 +0530

    add book schema

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
+import { create } from 'domain';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async findAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
+  @Post()
+  async createBook(
+    @Body()
+    book,
+  ): Promise<Book> {
+    return this.bookService.create(book);
+  }
+}
--- src/book/schemas/book.schema.ts
+++ src/book/schemas/book.schema.ts
@@ -1,20 +1,28 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-@Schema({
-    timestamps:true
-})
-
-export class Book{
-
-    @Prop()
-    title:string;
-
-    @Prop()
-    author:string;
-
-    @Prop()
-    discription:string;
-
-    @Prop()
-    price:number;
+export enum Catagory {
+  CRIME = 'Crime',
+  CLASSIC = 'Classic',
+  GOD = 'God',
 }
+@Schema({
+  timestamps: true,
+})
+export class Book {
+  @Prop()
+  title: string;
+
+  @Prop()
+  author: string;
+
+  @Prop()
+  discription: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  catagory: Catagory;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
